4e6a8ea369098f450a64fa6ddf117782a8cdd7c8
Damien Soulard <damien.soulard@gmail.com>
2017-01-06 07:18:13 +0100

    :sparkles: add working gh page

---
+++ index.js
@@ -0,0 +1,34 @@
+var shiftIsDown = false
+const shiftKeycode = 16
+
+// Watch keyup event
+document.addEventListener('keyup', function(e) {
+  const key = document.querySelector('.key')
+  const keycode = document.querySelector('.keycode')
+
+  // Don't display shift keycode when another key is being pressed
+  if (shiftIsDown && keycode === shiftKeycode) return;
+
+  key.textContent = e.key || ''
+  keycode.textContent = e.which || e.keyCode
+})
+
+// Watch if shift key is down
+document.addEventListener('keydown', function(e) {
+  const keycode = e.which || e.keyCode
+  if (keycode === shiftKeycode) {
+    shiftIsDown = true
+  }
+})
+
+function createHtml() {
+  const html = `
+    <div class="content">
+      <label class="key">Type something!</label>
+      <label class="keycode"></label>
+    </div>
+  `
+  document.body.innerHTML = html
+}
+
+createHtml()
