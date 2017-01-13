ae8a5cadb6b4958a0674a3a1196bb6b0c4ce39ed
Damien Soulard <damien.soulard@gmail.com>
2017-01-13 09:46:54 +0100

    :lipstick: update UI

--- docs/index.js
+++ docs/index.js
@@ -1,32 +1,29 @@
-// use var to make it compatible with Safari
-var shiftIsDown = false
-const shiftKeycode = 16
 
 // Watch keyup event
 document.addEventListener('keyup', function(e) {
   const key = document.querySelector('.key')
   const keycode = document.querySelector('.keycode')
 
-  // Don't display shift keycode when another key is being pressed
-  if (shiftIsDown && keycode === shiftKeycode) return;
-
   key.textContent = e.key || ''
   keycode.textContent = e.which || e.keyCode
-})
 
-// Watch if shift key is down
-document.addEventListener('keydown', function(e) {
-  const keycode = e.which || e.keyCode
-  if (keycode === shiftKeycode) {
-    shiftIsDown = true
+  // Handle space case
+  if (keycode.textContent == 32) {
+    key.textContent = "Space"
+  }
+
+  if (key.textContent === '') {
+    key.classList.add('hide')
+  } else {
+    key.classList.remove('hide')
   }
 })
 
 function createHtml() {
   const html = `
     <div class="content">
-      <label class="key">Type something!</label>
-      <label class="keycode"></label>
+      <div class="keycode">Type something!</div>
+      <div class="key hide"></div>
     </div>
   `
   document.body.innerHTML = html
